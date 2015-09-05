a4a1e45a3f48fd4c31c1c30e634368ec9c03cf5b
Craig Steddy <csteddy@petaplex.com>
2015-09-06 07:09:51 +0800

    remove my hack

--- src/lunajson.lua
+++ src/lunajson.lua
@@ -4,20 +4,6 @@ local sax = require 'lunajson.sax'
 -- If you need multiple contexts of decoder and/or encoder,
 -- you can require lunajson.decoder and/or lunajson.encoder directly.
 
-json = {}
-
-function json.parse(str)
-
-return newdecoder(str)
-
-end
-
-function json.stringify(table)
-
-return newencoder(table)
-
-end
-
 return {
 	decode = newdecoder(),
 	encode = newencoder(),
